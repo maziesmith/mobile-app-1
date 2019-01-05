10c3a5190dd2096bfc67cbd6c14d325628b40f61
oanacaplescu <oana.caplescu@gmail.com>
2019-01-05 16:15:27 +0100

    Fixed bug in which json data was not retrieved

--- app/src/main/java/com/example/oanac/recipes/ApiUtil.java
+++ app/src/main/java/com/example/oanac/recipes/ApiUtil.java
@@ -1,4 +1,55 @@
 package com.example.oanac.recipes;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class ApiUtil {
+    private ApiUtil(){}
+
+    public static final String API_URL =
+            "https://www.googleapis.com/books/v1/volumes/";
+
+    public static URL buildApiUrl(String title) {
+        String fullApiUrl = API_URL + "?q=" + title;
+        URL url = null;
+        try {
+            url = new URL(fullApiUrl);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getJson(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasData = scanner.hasNext();
+            if (hasData) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        }
+        catch (Exception e) {
+            Log.d("Error",
+                    e.toString());
+            return null;
+        }
+        finally {
+            connection.disconnect();
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/oanac/recipes/RecipesListActivity.java
+++ app/src/main/java/com/example/oanac/recipes/RecipesListActivity.java
@@ -1,7 +1,12 @@
 package com.example.oanac.recipes;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.net.URL;
 
 public class RecipesListActivity extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class RecipesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_list);
+        try {
+            URL bookUrl = ApiUtil.buildApiUrl("cooking");
+            new QueryTask().execute(bookUrl);
+        }
+        catch (Exception e) {
+            Log.d("Error", e.getMessage());
+        }
+
+
+    }
+
+    public class QueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchUrl);
+            }
+            catch (Exception e) {
+                Log.e("Error", e.getMessage());
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            TextView tvResult = (TextView) findViewById(R.id.mainTextView);
+            tvResult.setText(result);
+        }
     }
 }
+
+

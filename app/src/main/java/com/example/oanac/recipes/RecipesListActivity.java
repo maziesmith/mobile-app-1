394079400b29b69c423efe2f777496fc227065e9
oanacaplescu <oana.caplescu@gmail.com>
2019-01-05 12:31:01 +0100

    Created Android Studio project

---
+++ app/src/main/java/com/example/oanac/recipes/RecipesListActivity.java
@@ -0,0 +1,13 @@
+package com.example.oanac.recipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_list);
+    }
+}

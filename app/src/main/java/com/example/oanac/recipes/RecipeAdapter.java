cddac5e5e7795b68f511c534ef05547d85fc92e5
oanacaplescu <oana.caplescu@gmail.com>
2019-01-06 18:48:40 +0100

    Added Adapter for RecyclerView

--- app/src/main/java/com/example/oanac/recipes/RecipeAdapter.java
+++ app/src/main/java/com/example/oanac/recipes/RecipeAdapter.java
@@ -1,11 +1,39 @@
 package com.example.oanac.recipes;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class RecipeAdapter {
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+    ArrayList<Recipe> recipies;
+    public RecipeAdapter(ArrayList<Recipe> recipies) {
+        this.recipies = recipies;
+    }
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.list_item, viewGroup, false);
+        return new RecipeViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder recipeViewHolder, int currentPosition) {
+        Recipe recipe = recipies.get(currentPosition);
+        recipeViewHolder.bind(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipies.size();
+    }
 
     public class RecipeViewHolder extends RecyclerView.ViewHolder {
 
